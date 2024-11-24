554b2001a6640f69bf399f3ba107b7046a94dadb
farhamkh <fk61462n@pace.edu>
2024-11-24 14:57:04 -0500

    Two columns  Swipe Section and saved Outifts - npm reinstalled.

--- chicAI-frontend/src/components/OutfitManagement.js
+++ chicAI-frontend/src/components/OutfitManagement.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const OutfitManagement = () => {
   return (
-    <div>
-      <h1>Outfit Management</h1>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col md={4}>
+          <h2>Swipe Section</h2>
+        </Col>
+        <Col md={8}>
+          <h2>Saved Outfits</h2>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
 export default OutfitManagement;
+
