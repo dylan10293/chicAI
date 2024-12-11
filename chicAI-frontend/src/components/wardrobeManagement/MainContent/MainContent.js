cf730060652d60a1a435ef00c2950b171db960af
sharondsouza <sharondsouza26@gmail.com>
2024-12-11 15:35:34 -0500

    making item cards clickable and adding route for edit

--- chicAI-frontend/src/components/wardrobeManagement/MainContent/MainContent.js
+++ chicAI-frontend/src/components/wardrobeManagement/MainContent/MainContent.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './MainContent.css';
 import { Container, Stack, Card } from 'react-bootstrap';
 
@@ -8,28 +9,37 @@ function MainContent() {
     <Stack gap={3} className='wardrobe-management-content-stack'>
       <div className="p-2">Sweaters
       <Card className="wardrobe-management-card" style={{ width: '15rem' }}>
-      <Card.Img variant="top" src={require('./img/dummy-sweater.png')} />
+      <Link to="/details" state={{ item: 'Red Sweater', img: '/dummy-sweater.png' }}>
+      <Card.Img variant="top" src='/dummy-sweater.png' />
       <Card.Body>
         <Card.Text>Red Sweater</Card.Text>
       </Card.Body>
+      </Link>
         </Card>
       </div>
+
       <div className="p-2">Jackets
       <Card className="wardrobe-management-card" style={{ width: '15rem' }}>
-      <Card.Img variant="top" src={require('./img/dummy-jacket.png')} />
+      <Link to="/details" state={{ item: 'Yellow Jacket', img: '/dummy-jacket.png' }}>
+      <Card.Img variant="top" src='/dummy-jacket.png' />
       <Card.Body>
         <Card.Text>Yellow Jacket</Card.Text>
       </Card.Body>
+      </Link>
         </Card>
         </div>
+
       <div className="p-2">Tshirts
       <Card className="wardrobe-management-card" style={{ width: '15rem' }}>
-      <Card.Img variant="top" src={require('./img/dummy-shirt.png')} />
+      <Link to="/details" state={{ item: 'Green Tshirt', img: '/dummy-shirt.png' }}>
+      <Card.Img variant="top" src='/dummy-shirt.png' />
       <Card.Body>
         <Card.Text>Green Shirt</Card.Text>
       </Card.Body>
+      </Link>
         </Card>
         </div>
+
     </Stack>
     </Container>
   );
