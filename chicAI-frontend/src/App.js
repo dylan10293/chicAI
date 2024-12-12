012cb7eb16da500bc4f00bbbac264971bb5ca339
Dylan <dylan.10293@gmail.com>
2024-12-12 18:33:51 -0500

    Removed suggestions from app.js

--- chicAI-frontend/src/App.js
+++ chicAI-frontend/src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; // Ensure Bootstrap styles are loaded
 import OutfitManagement from "./components/outfitsManage/OutfitManagement";
-import Suggestions from "./components/outfitsManage/Suggestions";
 import OutfitCreator from "./components/outfitsManage/OutfitCreator";
 
 function App() {
-  // Fetch suggestions from the Suggestions logic
-  const suggestedOutfits = Suggestions();
 
   return (
     <div className="App">
       {/* Pass suggested outfits to OutfitManagement */}
       {/* <OutfitManagement suggestedOutfits={suggestedOutfits} /> */}
       {/* <OutfitManagement userId="user_2q0JuFr5SPDCEVSG3jdjarMRAFh" /> */}
-      <OutfitCreator userId="user_2q0a6hxnhL1ou9w5PlVpD1P8wXh"/>
+      <OutfitCreator userId="user_2q0a6hxnhL1ou9w5PlVpD1P8wXh" />
 
     </div>
   );
