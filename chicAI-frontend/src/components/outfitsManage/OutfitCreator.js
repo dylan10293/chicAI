12a3b0fb6c8ae70e7825b7d28b608e34c5e7de9e
farhamkh <fk61462n@pace.edu>
2024-12-13 14:06:42 -0500

    Fixed header, Starting Integration of SuggestionGenerator

--- chicAI-frontend/src/components/outfitsManage/OutfitCreator.js
+++ chicAI-frontend/src/components/outfitsManage/OutfitCreator.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button, Form, Card } from "react-bootstrap";
 import axios from "axios";
 import { FaTrash } from "react-icons/fa";
+import SuggestionGenerator from "./SuggestionGenerator";
 import './OutfitCreator.css';
 
 
