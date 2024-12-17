4b810e7ef8385dab8fda46aaefe334c122846340
Dylan <dylan.10293@gmail.com>
2024-12-17 13:35:54 -0500

    Indicate page on navigation header

--- chicAI-frontend/src/components/NavigationBar/NavigationBar.js
+++ chicAI-frontend/src/components/NavigationBar/NavigationBar.js
@@ -2,9 +2,10 @@ import './NavigationBar.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function NavigationBar() {
+  const location = useLocation();
   return (
     <Container className='NavigationBar flex-column'>
       <Navbar collapseOnSelect expand="lg" className="navbar">
@@ -12,10 +13,34 @@ function NavigationBar() {
           <Navbar.Toggle style={{ borderColor: 'white' }} aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="navigation-content text-white">
-              <Nav.Link as={Link} to="/" style={{ color: 'white' }}>Home</Nav.Link>
-              <Nav.Link as={Link} to="/wardrobe-management" style={{ color: 'white' }}>Wardrobe Management</Nav.Link>
-              <Nav.Link as={Link} to="/outfits" style={{ color: 'white' }}>Outfits</Nav.Link>
-              <Nav.Link as={Link} to="/laundry" style={{ color: 'white' }}>Laundry</Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/"
+                style={{ color: location.pathname === '/' ? 'yellow' : 'white' }}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/wardrobe-management"
+                style={{ color: location.pathname === '/wardrobe-management' ? 'yellow' : 'white' }}
+              >
+                Wardrobe Management
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/outfits"
+                style={{ color: location.pathname === '/outfits' ? 'yellow' : 'white' }}
+              >
+                Outfits
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/laundry"
+                style={{ color: location.pathname === '/laundry' ? 'yellow' : 'white' }}
+              >
+                Laundry
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
